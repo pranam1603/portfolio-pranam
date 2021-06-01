5debec97b56384e2048920bca9c504a023589987
pranam jain <pranamjain02@gmail.com>
2021-06-01 16:06:11 +0530

    remove responsive bugs

--- src/components/tools.js
+++ src/components/tools.js
@@ -15,17 +15,17 @@ const tools = () => {
                 <div className="vsCode tools-div">
                     <h3><SiVisualstudiocode /></h3>
                     <h4><a href='https://code.visualstudio.com/' rel="noopener noreferrer" target='_blank'>Visual Studio Code</a></h4>
-                    <p>I'm only using this code editor and I'm <br />completely in love with this! See the next resource for my<br /> setup.</p>
+                    <p>I'm only using this code editor and I'm completely in love with this! See the next resource for my setup.</p>
                 </div>
                 <div className="vsCodeExt tools-div">
                     <h3><VscExtensions /></h3>
                     <h4><a href='https://github.com/pranam1603/vsCode-Extensions' rel="noopener noreferrer" target='_blank'>My Visual Studio Code Setup</a> </h4>
-                    <p>Check out exactly what theme, extensions and <br />settings I use for VSCode in the projects and<br /> for coding.</p>
+                    <p>Check out exactly what theme, extensions and settings I use for VSCode in the projects and for coding.</p>
                 </div>
                 <div className="codePen tools-div">
                     <h3><SiCodepen /></h3>
                     <h4><a href='https://codepen.io/' rel="noopener noreferrer" target='_blank'>Codepen</a></h4>
-                    <p>Codepen has become an essential tool for me to<br /> quickly test out some ideas or do some tests.<br /> I use it for testing APIs.</p>
+                    <p>Codepen has become an essential tool for me to quickly test out some ideas or do some tests. I use it for testing APIs.</p>
                 </div>
             </div>
         </div>
