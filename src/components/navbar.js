3521865af6d1262a7791c28dca79a7046cb583d8
pranam jain <pranamjain02@gmail.com>
2021-06-02 00:27:20 +0530

    edit nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,11 +4,9 @@ import { FaBars, FaTimes } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-    // const [checked, setChecked] = useState(false)
 
     return (<div className="navigation">
         <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
-
         <label htmlFor="navi-toggle" className="navigation__button">
             <FaBars />
         </label>
@@ -19,7 +17,7 @@ const Navbar = () => {
             <ul className="navigation__list">
                 {links.map(link => {
                     return (
-                        <li className="navigation__item" key={link.id}><Link className="navigation__link" to={link.url}><span>0{link.id}</span>{link.text}</Link></li>
+                        <li className="navigation__item" key={link.id}><a className="navigation__link" href={link.url} ><span>0{link.id}</span>{link.text}</a></li>
                     )
                 })}
             </ul>
