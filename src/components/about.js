77656e76c61553b34b034bc64796e518c31c4d7a
pranam1603 <67546158+pranam1603@users.noreply.github.com>
2021-11-25 14:44:46 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -13,8 +13,8 @@ const about = () => {
                         <h3>About Me</h3>
                         <h2>PRANAM JAIN</h2>
                     </div>
-                    <p>Hello My name is Pranam Jain, and I'm a bhopal-based freelancer. I am also worked in some companies . Currentky i'm working in a Turkey based Startup <i>MyDosTech</i>.</p>
-                    <p>I have a diverse set of skills, ranging from design, to HTML+CSS+Javascript +React. I also use Node.js and MongoDb in backend.</p>
+                    <p>Hello My name is Pranam Jain, and I'm a bhopal-based freelancer. I am also worked in some companies . Currently I am working as Software Developer Intern at <i>Star Catalysts</i>.</p>
+                    <p>I have a diverse set of skills, ranging from design, to HTML+CSS+Javascript+React. I also use Node.js and MongoDb in backend.</p>
                     <a href="/about" className="main-btn ">Know more</a>
                 </article>
             </div>
